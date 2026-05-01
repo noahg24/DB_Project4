b814df484419ad2b561dc0699c7193d4e7fe784a
noahg24 <dethaystack24@gmail.com>
2026-04-30 22:35:16 -0400

    All necessary files for running in Repo

--- obj/Debug/net9.0/Final Program.AssemblyInfo.cs
+++ obj/Debug/net9.0/Final Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661d0ab0b851edf1cd78a19f5d5901fd5ce1ab3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef28ca92e5ca9379b684566c64e7a2df7a7187a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
